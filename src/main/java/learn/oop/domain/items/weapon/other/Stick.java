145a940ee867c4b57a11fad2cf9293c7eb37c229
Nattapon Tongta <nattapon.t@bemplc.co.th>
2023-10-14 14:00:09 +0700

    refactor: remove import don't use

--- src/main/java/learn/oop/domain/items/weapon/other/Stick.java
+++ src/main/java/learn/oop/domain/items/weapon/other/Stick.java
@@ -2,7 +2,6 @@ package learn.oop.domain.items.weapon.other;
 
 import learn.oop.domain.items.weapon.Weapon;
 import learn.oop.domain.jobs.Job;
-import learn.oop.domain.jobs.novice.Novice;
 
 public class Stick extends Weapon {
     public Stick(String name, int attackScore, Job whoCanUse) {
--- src/main/java/learn/oop/domain/jobs/magician/Magician.java
+++ src/main/java/learn/oop/domain/jobs/magician/Magician.java
@@ -1,6 +1,5 @@
 package learn.oop.domain.jobs.magician;
 
-import learn.oop.domain.behavior.attack.Attackable;
 import learn.oop.domain.behavior.attack.type.magic.MagicAttack;
 import learn.oop.domain.jobs.Job;
 
